8ff16063c51e25c0afe91d2e56f4cfaadbe8423a
Chaima Tounsi Omezzine <chaima.tounsi-omezzine@telecomnancy.eu>
2021-12-14 19:06:53 +0100

    Modification des controleurs

--- Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/MenuController.java
+++ Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/MenuController.java
@@ -1,10 +1,34 @@
 package eu.telecomnancy.profrdv.client.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class MenuController implements Observateur{
 
     public MenuController(){
 
     }
+
+    @FXML
+    private void handleAccueilButton (ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void handlePersonalButton (ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void handlePlanningButton (ActionEvent actionEvent) {
+
+    }
+
+    @FXML
+    private void handleIdButton (ActionEvent actionEvent) {
+
+    }
+
 
     @Override
     public void update() {
