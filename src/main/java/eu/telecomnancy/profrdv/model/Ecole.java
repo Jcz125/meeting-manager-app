c56442af41bcfe49ee41f7df9284c652aa367d03
Céline Zhang <celine.zhang@telecomnancy.eu>
2021-12-14 13:31:22 +0100

    ajout des signatures

--- src/main/java/eu/telecomnancy/profrdv/model/Ecole.java
+++ src/main/java/eu/telecomnancy/profrdv/model/Ecole.java
@@ -1,4 +1,13 @@
 package eu.telecomnancy.profrdv.model;
 
 public class Ecole {
+    private String nom;
+
+    public Ecole(String nom) {
+
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
 }
