25c5c839ebb95bb5faaa07edb21ae4d5d4086d0f
Clément BAILLY-GRANDVAUX <clement.bailly-grandvaux@telecomnancy.eu>
2021-12-14 13:14:57 +0100

    [Model] Ajout du modèle pour les Salles

---
+++ src/main/java/eu/telecomnancy/profrdv/model/Salle.java
@@ -0,0 +1,37 @@
+package eu.telecomnancy.profrdv.server;
+
+public class Salle {
+    private int numero;
+    private int etage;
+    private String aile;
+
+    public Salle (int numero, int etage, String aile) {
+        this.numero = numero;
+        this.etage =  etage;
+        this.aile = aile;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public void setEtage(int etage) {
+        this.etage = etage;
+    }
+
+    public int getEtage() {
+        return etage;
+    }
+
+    public void setAile(String aile) {
+        this.aile = aile;
+    }
+
+    public String getAile() {
+        return aile;
+    }
+}
