27e8dc29f681ed51eedc2e3694bf8d1dfce6e168
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-12-14 10:59:47 +0100

    [ADD] assesseurs et région

--- src/main/java/eu/telecomnancy/profrdv/model/RendezVous.java
+++ src/main/java/eu/telecomnancy/profrdv/model/RendezVous.java
@@ -1,6 +1,6 @@
 package eu.telecomnancy.profrdv.model;
 
-import eu.telecomnancy.profrdv.model.states.*;
+import eu.telecomnancy.profrdv.model.states.EtatRendezVous;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -9,18 +9,79 @@ public class RendezVous {
     private LocalDateTime localDateTime;
     private ArrayList<Utilisateur> utilisateurs;
     private String description;
+    private String titre;
 
     private EtatRendezVous etatRendezVous;
 
 
-    public RendezVous(LocalDateTime localDateTime, ArrayList<Utilisateur> utilisateurs, String description) {
+    public RendezVous(LocalDateTime localDateTime, ArrayList<Utilisateur> utilisateurs, String titre, String description) {
         this.localDateTime = localDateTime;
         this.utilisateurs = utilisateurs;
         this.description = description;
+        this.titre = titre;
     }
 
 
+    //region état rendez-vous
     public void setState(EtatRendezVous etatRendezVous) {
         this.etatRendezVous = etatRendezVous;
     }
+
+
+    public void annuler() {
+        this.etatRendezVous.annuler();
+    }
+
+
+    public void confirmer() {
+        this.etatRendezVous.confirmer();
+    }
+
+
+    public void demande() {
+        this.etatRendezVous.demande();
+    }
+
+
+    public void realiser() {
+        this.etatRendezVous.realiser();
+    }
+    //endregion
+
+
+    //region assesseurs
+    public ArrayList<Utilisateur> getUtilisateurs() {
+        return utilisateurs;
+    }
+
+
+    public EtatRendezVous getEtatRendezVous() {
+        return etatRendezVous;
+    }
+
+
+    public LocalDateTime getLocalDateTime() {
+        return localDateTime;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+
+    public String getTitre() {
+        return titre;
+    }
+
+
+    public void setTitre(String titre) {
+        this.titre = titre;
+    }
+
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    //endregion
 }
