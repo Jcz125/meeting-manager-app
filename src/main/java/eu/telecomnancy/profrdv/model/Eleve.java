99b878230e89d118f08ab655993c51942780e9e6
Céline Zhang <celine.zhang@telecomnancy.eu>
2021-12-14 13:57:25 +0100

    modification du diagrammes de classes

--- src/main/java/eu/telecomnancy/profrdv/model/Eleve.java
+++ src/main/java/eu/telecomnancy/profrdv/model/Eleve.java
@@ -7,14 +7,4 @@ public class Eleve extends Utilisateur {
     public Eleve(String nom, String prenom, String email) {
         super(nom, prenom, email);
     }
-
-    public void prendreRDV(Eleve e, LocalDateTime date) {
-
-    }
-    public void prendreRDV(List<Eleve> eleves, LocalDateTime date) {
-
-    }
-    public void prendreRDV(List<Professeur> profs, List<Eleve> eleves, LocalDateTime date) {
-
-    }
 }
--- src/main/java/eu/telecomnancy/profrdv/model/Professeur.java
+++ src/main/java/eu/telecomnancy/profrdv/model/Professeur.java
@@ -1,5 +1,7 @@
 package eu.telecomnancy.profrdv.model;
 
+import java.time.LocalDateTime;
+
 public class Professeur extends Utilisateur {
     public Professeur(String nom, String prenom, String email) {
         super(nom, prenom, email);
