35bc7e408c5d99079b2d4b5d48083acc438eac19
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-12-14 15:16:27 +0100

    changement des noms de variables

--- Serveur/src/main/java/eu/telecomnancy/profrdv/server/model/RendezVous.java
+++ Serveur/src/main/java/eu/telecomnancy/profrdv/server/model/RendezVous.java
@@ -9,20 +9,20 @@ import java.util.HashMap;
 import java.util.Set;
 
 public class RendezVous {
-    private final LocalDateTime localDateTime;
-    private final HashMap<Utilisateur, Boolean> utilisateurHashmap; // on donne une pair pour attribuer une confirmation à tout le monde
+    private final LocalDateTime horaire;
+    private final HashMap<Utilisateur, Boolean> utilisateurs; // on donne une pair pour attribuer une confirmation à tout le monde
     private String description;
     private String titre;
 
     private EtatRendezVous etatRendezVous;
 
 
-    public RendezVous(LocalDateTime localDateTime, ArrayList<Utilisateur> utilisateurs, String titre, String description) {
-        this.localDateTime = localDateTime;
-        this.utilisateurHashmap = new HashMap<>();
+    public RendezVous(LocalDateTime horaire, ArrayList<Utilisateur> utilisateurs, String titre, String description) {
+        this.horaire = horaire;
+        this.utilisateurs = new HashMap<>();
 
         for (Utilisateur utilisateur : utilisateurs) {
-            this.utilisateurHashmap.put(utilisateur, false);
+            this.utilisateurs.put(utilisateur, false);
         }
 
         this.description = description;
@@ -33,7 +33,7 @@ public class RendezVous {
     public void notifier() {
         // on notifie tous les utilisateurs d'un changement d'état sauf quand il a été réalisé
         if (!(etatRendezVous instanceof Realise)) {
-            for (Utilisateur utilisateur : utilisateurHashmap.keySet()) {
+            for (Utilisateur utilisateur : utilisateurs.keySet()) {
                 utilisateur.notifier(this);
             }
         }
@@ -41,7 +41,7 @@ public class RendezVous {
 
 
     public void ajoutConfirmation(Utilisateur CurrentUtilisateur) {
-        utilisateurHashmap.put(CurrentUtilisateur, true);
+        utilisateurs.put(CurrentUtilisateur, true);
     }
 
 
@@ -74,7 +74,7 @@ public class RendezVous {
 
     //region assesseurs
     public Set<Utilisateur> getUtilisateurs() {
-        return this.utilisateurHashmap.keySet();
+        return this.utilisateurs.keySet();
     }
 
 
@@ -83,8 +83,8 @@ public class RendezVous {
     }
 
 
-    public LocalDateTime getLocalDateTime() {
-        return this.localDateTime;
+    public LocalDateTime getHoraire() {
+        return this.horaire;
     }
 
 
