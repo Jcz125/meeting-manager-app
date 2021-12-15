c03d2ca88fc9ada45d823b129e09f4bffd5084bc
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-12-15 19:08:29 +0100

    test Utilisateur

--- Serveur/src/main/java/eu/telecomnancy/profrdv/server/model/utilisateur/Professeur.java
+++ Serveur/src/main/java/eu/telecomnancy/profrdv/server/model/utilisateur/Professeur.java
@@ -1,35 +1,35 @@
 package eu.telecomnancy.profrdv.server.model.utilisateur;
 
-import eu.telecomnancy.profrdv.server.model.Disponibilite;
 import eu.telecomnancy.profrdv.server.model.RendezVous;
+import eu.telecomnancy.profrdv.server.model.disponibilite.Disponibilite;
 
-import java.util.ArrayList;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 public class Professeur extends Utilisateur {
-    private final ArrayList<Disponibilite> disponibilites;
+    private final Disponibilite disponibilites;
 
 
     public Professeur(String nom, String prenom, String email) {
         super(nom, prenom, email);
-        disponibilites = new ArrayList<>();
+        disponibilites = new Disponibilite();
     }
 
 
-    public void add(Disponibilite disponibilite) {
-        disponibilites.add(disponibilite);
+    public void add(DayOfWeek jour, LocalTime debut, LocalTime fin) {
+        disponibilites.add(jour, debut, fin);
+    }
+
+
+    public void add(boolean inclut, LocalDateTime debut, LocalDateTime fin) {
+        disponibilites.add(inclut, debut, fin);
     }
 
 
     public boolean estDisponible(RendezVous rendezVous) {
-        if (!this.RDVs.containsKey(rendezVous.getHoraire())) {
-            for (Disponibilite plage : this.disponibilites) {
-                if (plage.getDebut().isBefore(rendezVous.getHoraire())) {
-                    if (plage.getFin().isAfter((rendezVous.getHoraire()))) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        if (RDVs.containsKey(rendezVous.getHoraire()))
+            return false;
+        return disponibilites.estDisponible(rendezVous.getHoraire());
     }
 }
---
+++ Serveur/src/test/java/eu/telecomnancy/profrdv/server/utilisateur/UtilisateurTest.java
@@ -0,0 +1,124 @@
+package eu.telecomnancy.profrdv.server.utilisateur;
+
+import eu.telecomnancy.profrdv.server.model.Salle;
+import eu.telecomnancy.profrdv.server.model.utilisateur.Eleve;
+import eu.telecomnancy.profrdv.server.model.utilisateur.Professeur;
+import eu.telecomnancy.profrdv.server.model.utilisateur.Utilisateur;
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+
+import static eu.telecomnancy.junit.ReflectionAssertions.assertInstanceOf;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UtilisateurTest {
+    @Test
+    public void testSuperTypes() {
+        Utilisateur eleve = new Eleve(null, null, null);
+        assertInstanceOf("eu.telecomnancy.profrdv.server.model.utilisateur.Eleve", eleve);
+        assertInstanceOf("eu.telecomnancy.profrdv.server.model.utilisateur.Utilisateur", eleve);
+    }
+
+
+    @Test
+    public void testGetNom() {
+        String nom = "Dupont";
+        Utilisateur eleve = new Eleve(nom, null, null);
+        assertEquals(eleve.getNom(), nom);
+    }
+
+
+    @Test
+    public void testGetPrenom() {
+        String prenom = "Roger";
+        Utilisateur eleve = new Eleve(null, prenom, null);
+        assertEquals(eleve.getPrenom(), prenom);
+    }
+
+
+    @Test
+    public void testGetEmail() {
+        String email = "littleboy@outlook.com";
+        Utilisateur eleve = new Eleve(null, null, email);
+        assertEquals(eleve.getEmail(), email);
+    }
+
+
+    @Test
+    public void testSetGetTelephone() {
+        String telephone = "06 51 84 62 64";
+        Utilisateur eleve = new Eleve(null, null, null);
+        eleve.setTelephone(telephone);
+        assertEquals(eleve.getTelephone(), telephone);
+    }
+
+
+    @Test
+    public void testGetTelephone() {
+        String email = "littleboy@outlook.com";
+        Utilisateur eleve = new Eleve(null, null, email);
+        assertEquals(eleve.getEmail(), email);
+    }
+
+
+    @Test
+    public void testSetNom() {
+        String nom = "Dupont";
+        String nouveauNom = "Dubois";
+        Utilisateur eleve = new Eleve(nom, null, null);
+        eleve.setNom(nouveauNom);
+        assertEquals(eleve.getNom(), nouveauNom);
+    }
+
+
+    @Test
+    public void testSetPrenom() {
+        String prenom = "Roger";
+        String nouveauPrenom = "Jean";
+        Utilisateur eleve = new Eleve(null, prenom, null);
+        eleve.setPrenom(nouveauPrenom);
+        assertEquals(eleve.getPrenom(), nouveauPrenom);
+    }
+
+
+    @Test
+    public void testSetEmail() {
+        String email = "littleboy@outlook.com";
+        String nouveauEmail = "bigBoy03@gmail.com";
+        Utilisateur eleve = new Eleve(null, null, email);
+        eleve.setEmail(nouveauEmail);
+        assertEquals(eleve.getEmail(), nouveauEmail);
+    }
+
+
+    @Test
+    public void testAjouterRDV() {
+        Eleve jean = new Eleve("Dupont", "Jean", "jean.dupont@gmail.com");
+        Eleve george = new Eleve("Bubois", "George", "george.dubois@outlook.com");
+
+        Professeur gerald = new Professeur("Oster", "Gerald", "gerald.oster@telecomnancy.net");
+        Professeur christophe = new Professeur("Bouthier", "Christophe", "christophe.bouthier@telecomnancy.net");
+
+        LocalTime debut = LocalTime.of(8, 0);
+        LocalTime fin = LocalTime.of(12, 0);
+        gerald.add(DayOfWeek.WEDNESDAY, debut, fin);
+        christophe.add(DayOfWeek.THURSDAY, debut, fin);
+
+
+        ArrayList<Professeur> profs = new ArrayList<>();
+        profs.add(gerald);
+
+        ArrayList<Eleve> eleves = new ArrayList<>();
+        eleves.add(george);
+
+        Salle salle = new Salle(2, 1, "ouest");
+        LocalDateTime date1 = LocalDateTime.of(2021, 12, 15, 14, 20);
+        assertFalse(jean.prendreRDV(profs, eleves, salle, date1, "RDV", "Test"));
+
+        LocalDateTime date2 = LocalDateTime.of(2021, 12, 15, 9, 20);
+        assertTrue(jean.prendreRDV(profs, eleves, salle, date2, "RDV", "Test"));
+    }
+}
