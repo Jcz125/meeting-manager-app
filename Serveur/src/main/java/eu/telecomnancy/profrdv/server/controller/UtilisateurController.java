8f4d2ccc4443f4add3401d60fdf99203c66e60eb
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-12-15 18:24:53 +0100

    ajout nouvelle données dans UtilisateurController

--- Serveur/src/main/java/eu/telecomnancy/profrdv/server/controller/UtilisateurController.java
+++ Serveur/src/main/java/eu/telecomnancy/profrdv/server/controller/UtilisateurController.java
@@ -1,7 +1,5 @@
 package eu.telecomnancy.profrdv.server.controller;
 
-import eu.telecomnancy.profrdv.server.model.Ecole;
-import eu.telecomnancy.profrdv.server.model.data.EcoleData;
 import eu.telecomnancy.profrdv.server.model.data.UtilisateurData;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,10 +10,10 @@ public class UtilisateurController {
     @GetMapping("/utilisateur")
     public UtilisateurData getSalle(@RequestParam(value = "id") Integer id) {
         if (id == 0)
-            return new UtilisateurData(id, "Bouthier", "Christophe", "littlegirl@gmail.com", "3630", false, 0, true);
+            return new UtilisateurData(id, "Bouthier", "Christophe", "littlegirl@gmail.com", "3630", false, 0, true, null, null);
         if (id == 1)
-            return new UtilisateurData(id, "Oster", "Gérald", "littleboy@gmail.com", "3630", false, 0, true);
-        return new UtilisateurData(id, "BG", "Clément", "jeff.bezos@amazon.com", "3630", false, 0, false);
+            return new UtilisateurData(id, "Oster", "Gérald", "littleboy@gmail.com", "3630", false, 0, true, null, null);
+        return new UtilisateurData(id, "BG", "Clément", "jeff.bezos@amazon.com", "3630", false, 0, false, null, null);
     }
 
     /*@PostMapping("/ecole")
