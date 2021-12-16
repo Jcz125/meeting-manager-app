a6a4b427066e90c0c98c8f0fc97c4ce47d863d0f
Chaima Tounsi Omezzine <chaima.tounsi-omezzine@telecomnancy.eu>
2021-12-16 13:54:11 +0100

    controller Planning

--- Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/PlanningController.java
+++ Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/PlanningController.java
@@ -1,11 +1,16 @@
 package eu.telecomnancy.profrdv.client.controllers;
 
 
+import eu.telecomnancy.profrdv.client.model.Utilisateur;
+
 public class PlanningController implements Observateur{
 
-    public PlanningController(){
+    private Utilisateur u ;
 
+    public PlanningController(Utilisateur u){
+        this.u = u ;
     }
+
 
     @Override
     public void update() {
