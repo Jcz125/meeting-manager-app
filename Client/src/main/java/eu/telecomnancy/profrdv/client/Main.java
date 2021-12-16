ab4b071995c5bbc33e591f25bda42d7c98058ff2
Lucas Gouin <Lucas.Gouin@telecomnancy.eu>
2021-12-16 11:43:55 +0100

    Ajout fonctionnalité pour changer de fenêtre

--- Client/src/main/java/eu/telecomnancy/profrdv/client/Main.java
+++ Client/src/main/java/eu/telecomnancy/profrdv/client/Main.java
@@ -1,71 +1,77 @@
 package eu.telecomnancy.profrdv.client;
 
-import eu.telecomnancy.profrdv.client.controllers.MainController;
 import eu.telecomnancy.profrdv.client.controllers.MenuController;
-import eu.telecomnancy.profrdv.client.controllers.PlanningController;
-import eu.telecomnancy.profrdv.client.controllers.PriseRDVController;
-import eu.telecomnancy.profrdv.client.model.Ecole;
-import eu.telecomnancy.profrdv.client.model.Professeur;
-import eu.telecomnancy.profrdv.client.model.RendezVous;
-import eu.telecomnancy.profrdv.client.model.Utilisateur;
-import eu.telecomnancy.profrdv.client.model.disponibilite.DisponibiliteFixe;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Main extends Application {
     private MenuController mc;
 
+
     @Override
     public void start(Stage stage) throws IOException {
-        Ecole ecole = new Ecole();
-        List<Utilisateur> utilisateurs = ecole.getUtilisateurs();
-        for (Utilisateur u: utilisateurs) {
-            System.out.println("L'utilisateur : " + u.getNom() +  " " + u.getPrenom() + (u instanceof Professeur ? " est Prof" : " est Eleve"));
-            if (u instanceof Professeur) {
-                System.out.println("est un prof avec comme dispo fixe : ");
-                for (DisponibiliteFixe d: u.getDisponibiliteFixe()) {
-                    System.out.println("Le " + d.getJour() + " de " + d.getDebut() + " à " + d.getFin());
-                }
-            }
-            else
-                System.out.println("est un élève");
-            System.out.println("Et a comme RDV :");
-            for (RendezVous rdv: u.getRDVs()) {
-                System.out.println("\"" + rdv.getTitre() + "\"" + "  " + rdv.getHoraire());
-            }
-        }
+//        Ecole ecole = new Ecole();
+//        List<Utilisateur> utilisateurs = ecole.getUtilisateurs();
+//        for (Utilisateur u: utilisateurs) {
+//            System.out.println("L'utilisateur : " + u.getNom() +  " " + u.getPrenom() + (u instanceof Professeur ? " est Prof" : " est Eleve"));
+//            if (u instanceof Professeur) {
+//                System.out.println("est un prof avec comme dispo fixe : ");
+//                for (DisponibiliteFixe d: u.getDisponibiliteFixe()) {
+//                    System.out.println("Le " + d.getJour() + " de " + d.getDebut() + " à " + d.getFin());
+//                }
+//            }
+//            else
+//                System.out.println("est un élève");
+//            System.out.println("Et a comme RDV :");
+//            for (RendezVous rdv: u.getRDVs()) {
+//                System.out.println("\"" + rdv.getTitre() + "\"" + "  " + rdv.getHoraire());
+//            }
+//        }
 
         BorderPane panneau = new BorderPane() ;
 
-        FXMLLoader loader1 = new FXMLLoader();
-        loader1.setLocation(getClass().getResource("MenuEleve.fxml"));
-        loader1.setControllerFactory(iC->new MenuController());
-        panneau.setTop(loader1.load());
+        FXMLLoader menuLoader = new FXMLLoader(getClass().getResource("MenuEleve.fxml"));
+        menuLoader.setControllerFactory(iC -> new MenuController());
+        panneau.setTop(menuLoader.load());
 
-        Utilisateur utilisateur = utilisateurs.get(0);
-        FXMLLoader loader2 = new FXMLLoader();
-        loader2.setLocation(getClass().getResource("PriseRDV.fxml"));
-        loader2.setControllerFactory(iC->new PriseRDVController(utilisateur));
-        panneau.setBottom(loader2.load());
 
-//        FXMLLoader loader3 = new FXMLLoader();
-//        loader3.setLocation(getClass().getResource("Planning.fxml"));
-//        loader3.setControllerFactory(iC->new PlanningController());
-//        panneau.setCenter(loader3.load());
+        FXMLLoader priseRDVLoader = new FXMLLoader(getClass().getResource("PriseRDV.fxml"));
+        Parent priseRDVParent = priseRDVLoader.load();
+
+
+        FXMLLoader creationLoader = new FXMLLoader(getClass().getResource("Creation.fxml"));
+        Parent creationParent = creationLoader.load();
+
+
+        MenuController menuController = menuLoader.getController();
+
+        menuController.setPlanningScene(priseRDVParent);
+        menuController.setCreationScene(creationParent);
+
+        menuController.setPanneau(panneau);
+
+        panneau.setBottom(priseRDVParent);
+
+
+//        creationLoader.setControllerFactory(iC -> new MenuController());
+
+        //        FXMLLoader loader3 = new FXMLLoader();
+        //        loader3.setLocation(getClass().getResource("Planning.fxml"));
+        //        loader3.setControllerFactory(iC->new PlanningController());
+        //        panneau.setCenter(loader3.load());
 
         Scene scene = new Scene(panneau, 1400, 750);
         stage.setTitle("ProfRDV");
         stage.setScene(scene);
-
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
--- Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/MenuController.java
+++ Client/src/main/java/eu/telecomnancy/profrdv/client/controllers/MenuController.java
@@ -3,42 +3,51 @@ package eu.telecomnancy.profrdv.client.controllers;
 import eu.telecomnancy.profrdv.client.model.Utilisateur;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
 
-public class MenuController implements Observateur{
+public class MenuController implements Observateur {
 
     private MenuController mc;
-    private Utilisateur utilisateur ;
-    private String idToConnect ;
+    private Utilisateur utilisateur;
+    private String idToConnect;
 
-    public MenuController(){
+    private BorderPane panneau;
+    private Parent creation;
+    private Parent planning;
 
-    }
 
-    @FXML
-    private void handleAccueilButton (ActionEvent actionEvent) {
-
-    }
-
-    @FXML
-    private void handlePersonalButton (ActionEvent actionEvent) {
+    public MenuController() {
 
     }
 
+
     @FXML
-    private void handlePlanningButton (ActionEvent actionEvent) {
+    private void handleAccueilButton(ActionEvent actionEvent) {
 
     }
 
+
     @FXML
-    private void handleIdButton (ActionEvent actionEvent) {
-        String id = (String) JOptionPane.showInputDialog(new Component(){},
+    private void handlePersonalButton(ActionEvent actionEvent) {
+
+    }
+
+
+    @FXML
+    private void handlePlanningButton(ActionEvent actionEvent) {
+        panneau.setBottom(planning);
+    }
+
+
+    @FXML
+    private void handleIdButton(ActionEvent actionEvent) {
+        String id = (String) JOptionPane.showInputDialog(new Component() {
+                                                         },
                 "Identifiant",
                 "Identifiez-Vous",
                 JOptionPane.PLAIN_MESSAGE,
@@ -47,27 +56,39 @@ public class MenuController implements Observateur{
                 null);
 
         if (id != null) {
-            this.idToConnect = id ;
+            this.idToConnect = id;
         }
     }
+
 
     public String getIdToConnect() {
         return this.idToConnect;
     }
 
+
     @FXML
-    private void handleCreateButton (ActionEvent actionEvent) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader();
-//        fxmlLoader.setLocation(getClass().getResource("Creation.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-//        Stage stage = new Stage();
-//        stage.setTitle("Creation de compte");
-//        stage.setScene(scene);
-//        stage.show();
+    private void handleCreateButton(ActionEvent actionEvent) throws IOException {
+        panneau.setBottom(creation);
     }
+
+
+    public void setCreationScene(Parent parent) {
+        this.creation = parent;
+    }
+
+
+    public void setPlanningScene(Parent parent) {
+        this.planning = parent;
+    }
+
 
     @Override
     public void update() {
 
+    }
+
+
+    public void setPanneau(BorderPane panneau) {
+        this.panneau = panneau;
     }
 }
